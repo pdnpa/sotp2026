0788f7a3d8f2a1309fbbbcbfddab3e73ea7d2544
Ross Hume <ross.hume@peakdistrict.gov.uk>
2026-09-01 12:35:33 +0100

    Create PagePage component

---
+++ .vitepress/components/PagePage.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import { data } from '../reportdata.data.js'
+import DynamicComponent from "./DynamicComponent.vue";
+import DynamicContentType from "./DynamicContentType.vue";
+import ReferenceList from "./ReferenceList.vue";
+import { contentCollectionIsNotEmpty, getFirstImage } from '../utils.js'
+
+const defaultPage = {
+  title: "Default Page",
+  contents: [{type: "rte", content: "<p>This is a default page.</p>"}]
+}
+export default defineComponent({
+  name: "PagePage", // :)
+  components: {DynamicComponent, ReferenceList, DynamicContentType},
+  props: {
+    page_id: {type: Number, required: true}
+  },
+  data() {
+    return {
+      page: data.pages[this.page_id] || { ...defaultPage },
+      usedReferenceIds: []
+    }
+  },
+  provide() {
+    return {
+      registerReference: (id) => {
+        if (!this.usedReferenceIds.includes(id)) {
+          this.usedReferenceIds.push(id);
+        }
+      }
+    }
+  },
+})
+</script>
+
+<template>
+<div>
+
+  <h1>{{page.title}}</h1>
+
+  <div class="factor-section-block body-text pb-0" v-if="hasDescriptions"><h2 class="mb-0 mt-0" id="Description">Description</h2></div>
+  <div class="factor-section-block pt-0" v-if="hasDescriptions">
+    <div class="factor-data-elements">
+
+      <DynamicContentType :chunks="page.contents" contentFieldName="content"/>
+
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
